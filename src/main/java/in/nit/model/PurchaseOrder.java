7714f2b2e82027f77611e77b86e73cbb2488f059
Prasad <Prasad@DESKTOP-0K7II7Q>
2020-03-14 07:45:07 +0530

    clone commit

---
+++ src/main/java/in/nit/model/PurchaseOrder.java
@@ -0,0 +1,90 @@
+package in.nit.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "potab")
+public class PurchaseOrder {
+	@Id
+	@GeneratedValue
+	@Column(name = "pid")
+	private Integer pId;
+	@Column(name = "ordode")
+	private String orderCode;
+	@Column(name = "refnum")
+	private Integer refNum;
+	@Column(name = "qchk")
+	private String qltyChk;
+	@Column(name = "status")
+	private String status;
+	@Column(name = "note")
+	private String note;
+
+	public PurchaseOrder() {
+		super();
+	}
+
+	public PurchaseOrder(Integer pId) {
+		super();
+		this.pId = pId;
+	}
+
+	public Integer getpId() {
+		return pId;
+	}
+
+	public void setpId(Integer pId) {
+		this.pId = pId;
+	}
+
+	public String getOrderCode() {
+		return orderCode;
+	}
+
+	public void setOrderCode(String orderCode) {
+		this.orderCode = orderCode;
+	}
+
+	public Integer getRefNum() {
+		return refNum;
+	}
+
+	public void setRefNum(Integer refNum) {
+		this.refNum = refNum;
+	}
+
+	public String getQltyChk() {
+		return qltyChk;
+	}
+
+	public void setQltyChk(String qltyChk) {
+		this.qltyChk = qltyChk;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseOrder [pId=" + pId + ", orderCode=" + orderCode + ", refNum=" + refNum + ", qltyChk=" + qltyChk
+				+ ", status=" + status + ", note=" + note + "]";
+	}
+
+}
